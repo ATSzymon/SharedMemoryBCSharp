2a7fbb3d7454ae5628c38eeca097d105047dea6a
SzymonJ <s.jarek@atutnet.pl>
2020-03-30 13:41:40 +0200

    update o Strukturę danych

--- SharedMemoryB/Program.cs
+++ SharedMemoryB/Program.cs
@@ -3,57 +3,119 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
 
 namespace SharedMemoryB
 {
     class Program
     {
+        private const int MAX_VARIABLE = 65000;
+
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        struct My_Variable_Struct
+        {
+            UInt32 variable_number;
+            UInt32 prev_value;
+            UInt32 value;
+            byte variable_changed;
+            byte log_variable;
+            short variable_delta;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        struct Variable_Struct
+        {
+            public Variable_Struct(int count)
+                : this()
+            {
+                tab_str = new My_Variable_Struct[count];
+            }
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = MAX_VARIABLE)]
+            My_Variable_Struct[] tab_str;
+        }
+
+
+
         static void Main(string[] args)
         {
+
+            Variable_Struct my_var_tab = new Variable_Struct(MAX_VARIABLE);
+
             List<int> a = new List<int>();
-            
 
-            using(MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("14"))
+            while (true)
             {
-                var mStream = new MemoryStream();
-                var binFormatter = new BinaryFormatter();
+                using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("14"))
+                {
 
-                // Where 'objectBytes' is your byte array.
-                //mStream.Write(objectBytes, 0, objectBytes.Length);
+              
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
 
 
-                //var myObject = binFormatter.Deserialize(mStream) as YourObjectType;
 
-                Stopwatch stopwatch = Stopwatch.StartNew();
-
-                
+                    SharedMemoryVariable sharedMemoryVariable = new SharedMemoryVariable();
+                    My_Variable_Struct variable = new My_Variable_Struct();
                     using (MemoryMappedViewStream stream = mmf.CreateViewStream())
                     {
 
                         BinaryReader reader = new BinaryReader(stream);
-                    var dupaaa = reader.ReadBytes(4);
-                    int bb = BitConverter.ToInt32(dupaaa, 0);
-                    //var aa = reader.ReadInt32();
+                        //sharedMemoryVariable = (SharedMemoryVariable)binFormatter.Deserialize();
+                        var dupaaa = reader.ReadBytes(65200 * System.Runtime.InteropServices.Marshal.SizeOf(typeof(My_Variable_Struct)));
+
+                        int raw_size;
+
+                        raw_size = Marshal.SizeOf(my_var_tab);
+
+                        IntPtr ptr = Marshal.AllocHGlobal(raw_size);
+
+                        Marshal.Copy(dupaaa, 0, ptr, raw_size);
+
+                        my_var_tab = (Variable_Struct)Marshal.PtrToStructure(ptr, my_var_tab.GetType());
+
+                        Marshal.FreeHGlobal(ptr);
 
 
+                        /*
+                        var handle = GCHandle.Alloc(dupaaa, GCHandleType.Pinned);
+                        try
+                        {
+                            My_Variable_Struct s = (My_Variable_Struct)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(My_Variable_Struct));
+                        }
+                        finally
+                        {
+                            handle.Free();
+                        }
+                        //int bb = BitConverter.ToInt32(dupaaa, 0);
+                        //var aa = reader.ReadInt32();
 
+                        using (var mStream = new MemoryStream(dupaaa))
+                        {
+                            //var binFormatter = new BinaryFormatter();
+                            //My_Variable_Struct myObject = (My_Variable_Struct)binFormatter.Deserialize(mStream);
+                        };
 
-                    //reader.ReadBytes(0);
-                    //mStream.Write(reader.ReadBytes(221), 0, reader.ReadBytes(221).Length);
-                    //mStream.Position = 0;
-                    //var myObject = binFormatter.Deserialize(mStream) as List<int>;
+                        Thread.Sleep(20);
 
+                        //reader.ReadBytes(0);
+                        //mStream.Write(reader.ReadBytes(65200 * System.Runtime.InteropServices.Marshal.SizeOf(typeof(MyVariable))), 0, reader.ReadBytes(65200* System.Runtime.InteropServices.Marshal.SizeOf(typeof(MyVariable))).Length);
+                        //mStream.Position = 0;
+                        //MyVariable myObject = (MyVariable)binFormatter.Deserialize(mStream);
 
+    */
 
-                    //Console.WriteLine(reader.ReadInt32());
+                        //Console.WriteLine(reader.ReadInt32());
+                    }
+
+                    stopwatch.Stop();
                 }
-                
-                stopwatch.Stop();
-                Console.ReadKey();
+                //Console.ReadKey();
             }
 
-            
         }
     }
 }
---
+++ SharedMemoryB/SharedMemoryVariable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedMemoryB
+{
+    [Serializable]
+    class SharedMemoryVariable
+    {
+        UInt32 variable_number;
+        UInt32 prev_value;
+        UInt32 value;
+        byte variable_changed;
+        byte log_variable;
+        short variable_delta;
+    }
+}
